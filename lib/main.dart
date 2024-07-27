5e3e290b93f51d8d620a354cfc13e72e4071b7fe
allan-nganga <allen.nganga7@gmail.com>
2024-07-27 13:43:06 +0300

    Initial commit with login and signup screens

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:invoice_generator/screens/logins/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ lib/screens/logins/signup.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:invoice_generator/screens/logins/login.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          _header(context),
+          _inputFields(context),
+          _loginInfo(context),
+        ]),
+      ),
+    ));
+  }
+
+  _header(context) {
+    return Column(
+      children: [
+        Text(
+          "Create Account",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Enter details to get started"),
+      ],
+    );
+  }
+
+  _inputFields(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Username",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.person),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Email Address",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.email_outlined),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.password_outlined),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Retype Password",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.password_outlined),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
+        )
+      ],
+    );
+  }
+
+  _loginInfo(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Already have an account?"),
+        TextButton(onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+            );
+        }, child: Text("Login"))
+      ],
+    );
+  }
+}
